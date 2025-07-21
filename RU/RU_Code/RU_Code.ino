ea881414879edfdb23ca19abf4a14871c57c94b8
GABRIEL SILVA <87396003+GSNGABRIEL@users.noreply.github.com>
2025-07-21 15:45:40 -0300

    Alteração da lógica do arduino

--- RU/RU_Code/RU_Code.ino
+++ RU/RU_Code/RU_Code.ino
@@ -1,3 +1,4 @@
+
 // ----------- Declaração dos pinos dos 9 botões -----------
 const int botoes[3][3] = {
   { 2, 3, 4 },  // carne vermelha:  ótimo, bom, ruim
@@ -17,12 +18,12 @@ unsigned int votosTotal = 0;       // soma global
 bool estadoAnterior[3][3] = { 0 };
 
 // ----------- Difinir Saida do LED -----------------
-const int LED = 11;
+const int SOM = 49;
 
 void setup() {
   Serial.begin(9600);
 
-  pinMode(LED, OUTPUT);
+  pinMode(SOM, OUTPUT);
 
   for (int i = 0; i < 3; i++) {
     for (int j = 0; j < 3; j++) {
@@ -33,49 +34,39 @@ void setup() {
 }
 
 void loop() {
+  int botoesPressionados = 0;
+  int pratoSelecionado = -1;
+  int avaliacaoSelecionada = -1;
+
   for (int i = 0; i < 3; i++) {
     for (int j = 0; j < 3; j++) {
       bool estadoAtual = digitalRead(botoes[i][j]);
 
       //------ Se houve transição HIGH→LOW  (botão pressionado) ------
       if (estadoAnterior[i][j] == HIGH && estadoAtual == LOW) {
-        votos[i][j]++;
-        votosTotal++;
-        enviarVotosSerial();
-        som();
-        delay(200);
+        botoesPressionados++;
+        pratoSelecionado=i;
+        avaliacaoSelecionada=j;
       }
       estadoAnterior[i][j] = estadoAtual;
     }
   }
+    if (botoesPressionados == 1) {
+    enviarVotosSerial(pratoSelecionado, avaliacaoSelecionada);
+    som();
+    delay(200);  // debounce
+  }
 }
 
 void som() {
-  digitalWrite(LED, HIGH);
-  delay(50);
-  digitalWrite(LED, LOW);
+  digitalWrite(SOM, HIGH);
+  delay(200);
+  digitalWrite(SOM, LOW);
 }
 
-// ----------- Envia todos os contadores via Serial em JSON -----------
-void enviarVotosSerial() {
-  for (int i = 0; i < 3; i++) {
-    int totalPrato = votos[i][0] + votos[i][1] + votos[i][2];
-
-    for (int j = 0; j < 3; j++) {
-      double porcento = (totalPrato > 0) ? (votos[i][j] * 100.0) / totalPrato : 0.0;
-
-      Serial.print(pratos[i]);
-      Serial.print("_");
-      Serial.print(avaliacoes[j]);
-      Serial.print(", ");
-      Serial.print(votos[i][j]);
-      Serial.print(", ");
-      Serial.print(porcento, 2);
-      Serial.print("; ");
-    }
-  }
-
-  // Linha final com total global
-  Serial.print(" total: ");
-  Serial.print(votosTotal);
+// ----------- Envia os dados via Serial -----------
+void enviarVotosSerial(int prato, int avaliacao) {
+  Serial.print(pratos[prato]);
+  Serial.print("_");
+  Serial.print(avaliacoes[avaliacao]);
 }
